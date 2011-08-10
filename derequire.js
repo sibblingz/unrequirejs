f8b1dcb95f6a478220131afec7ef601bf6e85ccf
Matt Glazar <matt.glazar@gmail.com>
2011-08-10 15:34:39 -0700

    Simple derequire method

--- derequire.js
+++ derequire.js
@@ -29,12 +29,71 @@ var output = process.stdout;
 
 switch (level) {
     case 'SIMPLE':
-        throw new Error('SIMPLE level not supported');
+        simple(output);
+        break;
     case 'ADVANCED':
         advanced(output);
         break;
     default:
         throw new Error('Unknown level: ' + level);
+}
+
+function simple(output) {
+    var vm = require('vm');
+    var fs = require('fs');
+
+    var codes = { };
+    var writeCodeKey = 'write_code_derequire_internal_stuff';
+
+    var sandbox = {
+        require: null,
+        define: null
+    };
+
+    sandbox[writeCodeKey] = function writeCode(scriptName) {
+        output.write(codes[scriptName] + '\n');
+        delete codes[scriptName];
+    };
+
+    var r = require('./unrequire');
+    r.require.env({
+        context: sandbox,
+        init: function (require, define, env) {
+            sandbox.require = require;
+            sandbox.define = define;
+        },
+        loadScriptSync: function (scriptName) {
+            var code = fs.readFileSync(scriptName, 'utf8');
+            codes[scriptName] = code;
+
+            // I know, I know.  This is super flaky.  Sorry.
+            var callsRe = '';
+            callsRe += '\\b(require|define)[\\s\\r\\n]*'; // require or define call
+            callsRe += '\\([^,]*?';                       // First parameter
+            callsRe += '(,[^,]+)?';                       // Optional second parameter
+            callsRe += ',[\\s\\r\\n]*\\[[^]*?\\]';        // Dependency list
+            // Extra step so we can split up our RE
+            callsRe = new RegExp(callsRe, 'g');
+
+            var calls = code.match(callsRe) || [ ];
+            calls.forEach(function (call, i) {
+                call = call + ', function () {'
+                    + writeCodeKey + '(' + JSON.stringify(scriptName) + ');'
+                    + '})';
+
+                vm.runInNewContext(call, sandbox, scriptName + ':' + i);
+            });
+
+            return true;
+        }
+    });
+
+    output.write('(function () {\n');
+    output.write(fs.readFileSync(path.join(__dirname, 'unrequire.js')) + '\n');
+    r.require({
+        baseUrl: baseUrl
+    }, scriptFiles);
+    output.write('})();\n');
 }
 
 function advanced(output) {
