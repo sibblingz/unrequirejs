aad76f7b7bf581a7da69fb76a3c1b5d32e41113b
Matt Glazar <matt.glazar@gmail.com>
2011-08-10 16:00:27 -0700

    Refactor and handle trailing comments

--- derequire.js
+++ derequire.js
@@ -1,5 +1,118 @@
 #!/usr/bin/env node
 
+var END_SCRIPT = '\n//*/\n';
+
+function simple(output) {
+    var vm = require('vm');
+    var fs = require('fs');
+
+    var writeCodeKey = 'write_code_derequire_internal_stuff';
+
+    var sandbox = {
+        require: null,
+        define: null
+    };
+
+    sandbox[writeCodeKey] = function writeCode(code) {
+        output.write(code + END_SCRIPT);
+    };
+
+    // I know, I know.  This is super flaky.  Sorry.
+    var callsRe = '';
+    callsRe += '\\b(require|define)[\\s\\r\\n]*\\('; // require or define call
+    callsRe += '(,[^,]+)?';                          // Optional first parameter
+    callsRe += '(,[^,]+)?';                          // Optional second parameter
+    callsRe += '[\\s\\r\\n]*\\[[^]*?\\]';            // Dependency list
+    // Extra step so we can split up our RE
+    callsRe = new RegExp(callsRe, 'g');
+
+    function rewriteCalls(code) {
+        return code;
+    }
+
+    function getCalls(code) {
+        var calls = code.match(callsRe) || [ ];
+        return calls.map(function (call) {
+            return call + ', function () {'
+                + writeCodeKey + '(' + JSON.stringify(code) + ');'
+                + '})';
+        });
+    }
+
+    var r = require('./unrequire');
+    r.require.env({
+        context: sandbox,
+        init: function (require, define, env) {
+            sandbox.require = require;
+            sandbox.define = define;
+        },
+        loadScriptSync: function (scriptName) {
+            var code = fs.readFileSync(scriptName, 'utf8');
+            code = rewriteCalls(code);
+
+            getCalls(code).forEach(function (call, i) {
+                vm.runInNewContext(call, sandbox, scriptName + ':' + i);
+            });
+
+            return true;
+        }
+    });
+
+    output.write('(function () {' + END_SCRIPT);
+    output.write(fs.readFileSync(path.join(__dirname, 'unrequire.js')) + END_SCRIPT);
+    r.require({
+        baseUrl: baseUrl
+    }, scriptFiles);
+    output.write('})();' + END_SCRIPT);
+}
+
+function advanced(output) {
+    var vm = require('vm');
+
+    var sandbox = {
+        require: null,
+        define: null
+    };
+
+    function getScriptVariableName(scriptName) {
+        // TODO More robust solution
+        return scriptName.replace(/[^a-z_$]/ig, '_');
+    }
+
+    var r = require('./unrequire');
+    r.require.env({
+        context: sandbox,
+        init: function (require, define, env) {
+            sandbox.require = require;
+            sandbox.define = define;
+        },
+        userCallback: function (scriptName, callback, moduleValues, moduleScripts) {
+            if (!callback) return;
+
+            var fn = callback.toString();
+            var argNames = /\((.*?)\)/.exec(fn)[1].split(/[,\s]+/g);
+            var argValues = moduleScripts.map(getScriptVariableName);
+            var body = fn.substr(fn.indexOf('{') + 1).replace(/}[\s\r\n]*$/g, '');
+
+            // XXX HACK HACK HACK XXX Remove require, exports, module
+            argValues.splice(-3, 3);
+
+            output.write('var ' + getScriptVariableName(scriptName) + ' = ');
+            output.write('(function (' + argNames.join(', ') + ') {' + END_SCRIPT);
+            output.write(body + END_SCRIPT);
+            output.write('})(' + argValues.join(', ') + ');' + END_SCRIPT);
+
+            return null;
+        }
+    });
+
+    output.write('(function () {' + END_SCRIPT);
+    r.require({
+        baseUrl: baseUrl
+    }, scriptFiles);
+    output.write('})();' + END_SCRIPT);
+}
+
 var path = require('path');
 var args = require('optimist').argv;
 
@@ -36,109 +149,4 @@ switch (level) {
         break;
     default:
         throw new Error('Unknown level: ' + level);
-}
-
-function simple(output) {
-    var vm = require('vm');
-    var fs = require('fs');
-
-    var codes = { };
-    var writeCodeKey = 'write_code_derequire_internal_stuff';
-
-    var sandbox = {
-        require: null,
-        define: null
-    };
-
-    sandbox[writeCodeKey] = function writeCode(scriptName) {
-        output.write(codes[scriptName] + '\n');
-        delete codes[scriptName];
-    };
-
-    var r = require('./unrequire');
-    r.require.env({
-        context: sandbox,
-        init: function (require, define, env) {
-            sandbox.require = require;
-            sandbox.define = define;
-        },
-        loadScriptSync: function (scriptName) {
-            var code = fs.readFileSync(scriptName, 'utf8');
-            codes[scriptName] = code;
-
-            // I know, I know.  This is super flaky.  Sorry.
-            var callsRe = '';
-            callsRe += '\\b(require|define)[\\s\\r\\n]*'; // require or define call
-            callsRe += '\\([^,]*?';                       // First parameter
-            callsRe += '(,[^,]+)?';                       // Optional second parameter
-            callsRe += ',[\\s\\r\\n]*\\[[^]*?\\]';        // Dependency list
-            // Extra step so we can split up our RE
-            callsRe = new RegExp(callsRe, 'g');
-
-            var calls = code.match(callsRe) || [ ];
-            calls.forEach(function (call, i) {
-                call = call + ', function () {'
-                    + writeCodeKey + '(' + JSON.stringify(scriptName) + ');'
-                    + '})';
-
-                vm.runInNewContext(call, sandbox, scriptName + ':' + i);
-            });
-
-            return true;
-        }
-    });
-
-    output.write('(function () {\n');
-    output.write(fs.readFileSync(path.join(__dirname, 'unrequire.js')) + '\n');
-    r.require({
-        baseUrl: baseUrl
-    }, scriptFiles);
-    output.write('})();\n');
-}
-
-function advanced(output) {
-    var vm = require('vm');
-
-    var sandbox = {
-        require: null,
-        define: null
-    };
-
-    function getScriptVariableName(scriptName) {
-        // TODO More robust solution
-        return scriptName.replace(/[^a-z_$]/ig, '_');
-    }
-
-    var r = require('./unrequire');
-    r.require.env({
-        context: sandbox,
-        init: function (require, define, env) {
-            sandbox.require = require;
-            sandbox.define = define;
-        },
-        userCallback: function (scriptName, callback, moduleValues, moduleScripts) {
-            if (!callback) return;
-
-            var fn = callback.toString();
-            var argNames = /\((.*?)\)/.exec(fn)[1].split(/[,\s]+/g);
-            var argValues = moduleScripts.map(getScriptVariableName);
-            var body = fn.substr(fn.indexOf('{') + 1).replace(/}[\s\r\n]*$/g, '');
-
-            // XXX HACK HACK HACK XXX Remove require, exports, module
-            argValues.splice(-3, 3);
-
-            output.write('var ' + getScriptVariableName(scriptName) + ' = ');
-            output.write('(function (' + argNames.join(', ') + ') {\n');
-            output.write(body + '\n');
-            output.write('})(' + argValues.join(', ') + ');\n');
-
-            return null;
-        }
-    });
-
-    output.write('(function () {\n');
-    r.require({
-        baseUrl: baseUrl
-    }, scriptFiles);
-    output.write('})();\n');
 }
