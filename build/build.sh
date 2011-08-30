9595a8ca312f51e63d5068ab5b30ca10074fc436
Matt Glazar <matt.glazar@gmail.com>
2011-08-30 15:58:08 -0700

    Spaceport support

--- build/build.sh
+++ build/build.sh
@@ -29,6 +29,7 @@ options:
   --help             Print this help
   --browser          Build only with browser support
   --nodejs           Build only with Node.JS support
+  --spaceport        Build only with Spaceport support
   --enable-commonjs  Build only with CommonJS compatibility
   --enable-aliases   Build only with alias support
   --output file      Specify the output file
@@ -40,6 +41,7 @@ OPT_ENABLE_COMMONJS=false
 OPT_ENABLE_ALIASES=false
 OPT_BROWSER=false
 OPT_NODEJS=false
+OPT_SPACEPORT=false
 
 OPT_ANY=false
 OPT_OUT_GIVEN=false
@@ -48,8 +50,9 @@ while [ "$#" -gt 0 ]; do
     case "$1" in
         --help) print_usage ; exit 0 ;;
 
-        --browser) OPT_ANY=true ; OPT_BROWSER=true ;;
-        --nodejs)  OPT_ANY=true ; OPT_NODEJS=true ;;
+        --browser)   OPT_ANY=true ; OPT_BROWSER=true ;;
+        --nodejs)    OPT_ANY=true ; OPT_NODEJS=true ;;
+        --spaceport) OPT_ANY=true ; OPT_SPACEPORT=true ;;
 
         --enable-commonjs) OPT_ANY=true ; OPT_ENABLE_COMMONJS=true ;;
         --enable-aliases)  OPT_ANY=true ; OPT_ENABLE_ALIASES=true ;;
@@ -66,6 +69,7 @@ if ! $OPT_ANY; then
     OPT_ENABLE_ALIASES=true
     OPT_BROWSER=true
     OPT_NODEJS=true
+    OPT_SPACEPORT=true
 fi
 
 (
@@ -77,6 +81,7 @@ fi
     echo "/**@const*/ var ENABLE_ALIASES = $OPT_ENABLE_ALIASES;"
     echo "/**@const*/ var ENABLE_BROWSER = $OPT_BROWSER;"
     echo "/**@const*/ var ENABLE_NODEJS = $OPT_NODEJS;"
+    echo "/**@const*/ var ENABLE_SPACEPORT = $OPT_SPACEPORT;"
     echo "/**@const*/ var BROWSER_SYNC = false;"
     echo "/**@const*/ var ENABLE_PACKAGES = true;"
     echo "/**@const*/ var LOGGING = false;"
