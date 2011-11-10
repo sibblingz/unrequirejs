9a8d237cffe5c791321846cae1a35ab421567714
Matt Glazar <matt.glazar@gmail.com>
2011-11-10 13:57:38 -0800

    Simple stress test with cwd

--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,9 @@
 var test = (function () {
     var failed = 0;
     var run = 0;
+    var completed = false;
 
-    return {
+    var test = {
         assert: function assert(condition, message) {
             ++run;
 
@@ -21,12 +22,30 @@ var test = (function () {
                 throw new Error(fullMessage);
             }
         },
+
+        assertEqual: function assertEqual(expected, actual, message) {
+            var supplement = "expected: " + expected + "; actual: " + actual;
+            test.assert(expected === actual, message ? message + " (" + supplement + ")" : supplement);
+        },
+
+        step: function step(name) {
+            console.info("Test step " + name);
+        },
+
         done: function done() {
+            if (completed) {
+                throw new Error("Tests already completed");
+            }
+
+            completed = true;
+
             if (failed) {
                 console.warn("All tests completed; " + failed + " of " + run + " assertions failed");
             } else {
-                (console.info || console.log)("All " + run + " assertions passed");
+                console.info("All " + run + " assertions passed");
             }
         }
     };
+
+    return test;
 }());
