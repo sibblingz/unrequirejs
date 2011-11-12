4e9399dc3fcfcfd0b6536d8498db979e007e845b
Matt Glazar <matt.glazar@gmail.com>
2011-11-11 19:47:28 -0800

    More aggressive compression

--- build/build.sh
+++ build/build.sh
@@ -7,14 +7,14 @@ OUT="$ROOT/unrequire.min.js"
 function minify_closure_compiler {
     # Minify with Google Closure Compiler
     type java > /dev/null 2>&1 &&
-        java -jar "$DIR/google-closure-compiler-1180.jar" --compilation_level SIMPLE_OPTIMIZATIONS ||
+        java -jar "$DIR/google-closure-compiler-1180.jar" --compilation_level ADVANCED_OPTIMIZATIONS ||
         (echo 'WARNING: Java not installed; skipping Google Closure Compiler minification' >&2; cat)
 }
 
 function minify_uglifyjs {
     # Minify with UglifyJS
     type uglifyjs > /dev/null 2>&1 &&
-        uglifyjs ||
+        uglifyjs --no-seqs ||
         (echo 'WARNING: UglifyJS not installed; skipping UglifyJS minification' >&2; cat)
 }
 
@@ -46,7 +46,7 @@ done
 
 (
     echo ';// I am awesome'
-    echo '(function () {'
+    echo '(function (window) {'
 
     # Flags
     echo "/**@const*/ var ENABLE_PACKAGES = true;"
@@ -59,9 +59,20 @@ done
     # Plugins
     cat "$ROOT/lib/browser.js"
 
+    echo '}(window));'
+) | (
+    minify_closure_compiler
+) | (
+    # For whatever reason, Closure decides it's okay to pollute the global
+    # namespace with a `null` variable.  Better safe than sorry!
+    echo ';// I am awesome'
+    echo '(function () {'
+    cat
     echo '}());'
 ) | (
-    minify_closure_compiler | minify_uglifyjs
+    minify_uglifyjs
 ) > "$OUT"
+
+uglifyjs -b -ns -nm < "$OUT" > "$OUT.unmin.js"
 
 $OPT_OUT_GIVEN || echo "Build done; see $OUT"
